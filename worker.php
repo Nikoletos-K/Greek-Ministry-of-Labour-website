9073bc854ac3016fa6f3e6faa7052ec306b31d5f
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2021-01-08 14:39:39 +0200

    start worker page

--- worker.php
+++ worker.php
@@ -63,6 +63,137 @@
         </div>
         </section><!-- End Breadcrumbs -->
 
+     <div class="col-lg-10 mt-4 mt-lg-0">   
+        <div class="tab-wrapper">
+            <ul class="tab-menu">
+                <li class="active">Κάνε αίτηση</li>
+                <li>Εργασιακή Κατάσταση </li>
+                <li>Λεωφορεία</li>
+                <li>Τραμ</li>
+                <li>Προαστιακός</li>
+                <li>Τρόλεϊ</li>
+            </ul>
+            <div class="tab-content">
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <a href="timetables_results.php?TID=<?php echo 'Μ1'; ?>" class="icon-border-m1">
+                        <span>M1</span>
+                    </a>
+                </div>
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <a href="timetables_results.php?TID=<?php echo 'Μ2'; ?>" class="icon-border-m2">
+                        <span>M2</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-m3">
+                        <span>M3</span>
+                    </a>
+                </div>
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <p onClick = "openList(this)" id="l0" class="icon-border-l">
+                        <span>0</span>
+                    </p>
+                    <p onClick = "openList(this)" id="l1" class="icon-border-l">
+                        <span>1</span>
+                    </p>
+                    <p onClick = "openList(this)" id="l6" class="icon-border-l">
+                        <span>6</span>
+                    </p>
+                    <ul id="lil0" style="display: none">
+                        <li>
+                            <a href="coming_soon.php" class="icon-border-l">
+                                <span>022</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="timetables_results.php?TID=<?php echo '024'; ?>" class="icon-border-l">
+                                <span>024</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="coming_soon.php" class="icon-border-l">
+                                <span>049</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul id="lil1" style="display: none">
+                        <li>
+                            <a href="coming_soon.php" class="icon-border-l">
+                                <span>101</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="coming_soon.php" class="icon-border-l">
+                                <span>122</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="timetables_results.php?TID=<?php echo '140'; ?>" class="icon-border-l">
+                                <span>140</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul id="lil6" style="display: none">
+                        <li>
+                            <a href="coming_soon.php" class="icon-border-l">
+                                <span>602</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <a href="coming_soon.php" class="icon-border-tram">
+                        <span>T</span>
+                    </a>
+                </div>
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <a href="timetables_results.php?TID=<?php echo 'Π1'; ?>" class="icon-border-p1">
+                        <span>Π1</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-p2">
+                        <span>Π2</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-p3">
+                        <span>Π3</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-p4">
+                        <span>Π4</span>
+                    </a>
+                </div>
+                <div>
+                    <h5>Επιλέξτε μια γραμμή</h5>
+                    <br>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T1</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T2</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T3</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T4</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T5</span>
+                    </a>
+                    <a href="coming_soon.php" class="icon-border-t">
+                        <span>T6</span>
+                    </a>
+                </div>
+            </div><!-- //tab-content -->
+        </div><!-- //tab-wrapper -->
+    </div> 
+
 
 
 
@@ -100,6 +231,9 @@ include "./footer.php";
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <!-- MDB core JavaScript -->
 <script type="text/javascript" src="js/mdb.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="assets/js/wrapper.js"></script>
 
 
 </body>
