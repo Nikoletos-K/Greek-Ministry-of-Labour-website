32b655147bab0e46c30b1e68432f86c3551535f8
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2021-01-11 12:22:36 +0200

    db

--- sdi1700038.sql
+++ sdi1700038.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Jan 09, 2021 at 10:24 AM
+-- Generation Time: Jan 11, 2021 at 11:10 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -20,6 +20,88 @@ SET time_zone = "+00:00";
 --
 -- Database: `sdi1700038`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `firstname_surname` text NOT NULL,
+  `email` text NOT NULL,
+  `appointmentfor` text NOT NULL,
+  `date` text NOT NULL,
+  `appointmenthour` text NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`firstname_surname`, `email`, `appointmentfor`, `date`, `appointmenthour`, `message`) VALUES
+('', '', 'default', '', 'default', ''),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'director', '01/21/2021', '09:00', ''),
+('', '', 'director', '01/21/2021', '09:00', ''),
+('', '', 'default', '', 'default', ''),
+('', '', 'default', '', '', ''),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'minister', '01/14/2021', '09:00', '------------------'),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'director', '01/20/2021', '09:00', '---------------'),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'director', '01/21/2021', '09:00', '-----'),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'director', '01/05/2021', '09:00', '-------------'),
+('Konstantinos Nikoletos', 'nikoletos.konstantinos@gmail.com', 'director', '01/28/2021', '09:00', '--------------');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `afm` varchar(100) NOT NULL,
+  `workingFor` varchar(100) DEFAULT NULL,
+  `anastoli_id` varchar(100) DEFAULT NULL,
+  `thlergasia_id` varchar(100) DEFAULT NULL,
+  `adeia_id` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_action`
+--
+
+CREATE TABLE `employee_action` (
+  `action_id` varchar(100) NOT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `starting_date` varchar(100) NOT NULL,
+  `end_date` varchar(100) NOT NULL,
+  `number_of_children` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employer`
+--
+
+CREATE TABLE `employer` (
+  `username` varchar(100) NOT NULL,
+  `firstname` text NOT NULL,
+  `lastname` text NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `afm` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `employer`
+--
+
+INSERT INTO `employer` (`username`, `firstname`, `lastname`, `password`, `email`, `phone`, `afm`) VALUES
+('', '', '', '', '', '', '1234566');
 
 -- --------------------------------------------------------
 
@@ -43,12 +125,7 @@ CREATE TABLE `genericuser` (
 --
 
 INSERT INTO `genericuser` (`username`, `firstname`, `lastname`, `password`, `email`, `phone`, `afm`, `role`) VALUES
-('Konstantinos', 'Konstantinos', 'Nikoletos', '', 'nikoletos.konstantinos@gmail.com', '', '1234566', 'ergazomenos'),
-('KonstantinosN', 'Konstantinos', 'Nikoletos', '', 'nikoletos.konstant@gmail.com', '', '1234566', 'ergodoths'),
-('KonstantinosNik', 'Konstantinos', 'Nikoletos', 'b59c67bf196a4758191e42f76670ceba', 'nikoletos.konstntinos@gmail.com', '234552222222', '1234566', 'ergodoths'),
-('KonstantinosNNNNNNNNN', 'Konstantinos', 'Nikoletos', 'b59c67bf196a4758191e42f76670ceba', 'nikolos.konstantinos@gmail.com', '234552222222', '1234566', 'ergodoths'),
-('Myrto', 'Myrto', 'Igglezou', '827ccb0eea8a706c4c34a16891f84e7b', 'myrto.iglezou@gmail.com', '234552222222', '1234566', 'ergodoths'),
-('konstantinos', 'Konstantinos', 'Nikoletos', '827ccb0eea8a706c4c34a16891f84e7b', 'konstantinos@gmail.com', '234552222222', '1234566', 'ergazomenos');
+('konos', 'Konstantinos', 'Nikoletos', '827ccb0eea8a706c4c34a16891f84e7b', 'nikoletos.konstantinos@gmail.com', '234552222222', '1234566', 'ergodoths');
 
 -- --------------------------------------------------------
 
@@ -106,31 +183,47 @@ INSERT INTO `message` (`name`, `email`, `subject`, `message`) VALUES
 ('ΚΟΝΟΣ', 'nikoletos.konstantinos@gmail.com', 'THEMATARA', 'asa'),
 ('ΚΟΝΟΣ', 'nikoletos.konstantinos@gmail.com', 'THEMATARA', 'ssss'),
 ('ΚΟΝΟΣ', 'nikoletos.konstantinos@gmail.com', 'THEMATARA', 'asssssssss'),
-('', '', '', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `simpleuser`
---
-
-CREATE TABLE `simpleuser` (
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `phone` varchar(100) NOT NULL,
-  `afm` varchar(100) NOT NULL,
-  `role` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+('', '', '', ''),
+('ΚΟΝΟΣ', 'nikoletos.konstantinos@gmail.com', 'THEMATARA', '----------'),
+('ΚΟΝΟΣ', 'nikoletos.konstantinos@gmail.com', 'THEMATARA', '----------');
 
 --
--- Dumping data for table `simpleuser`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `simpleuser` (`username`, `password`, `email`, `phone`, `afm`, `role`) VALUES
-('Ko', '25f9e794323b453885f5181f1b624d0b', 'niko@letos', '', '1234566', 'Κατάσταση'),
-('Ko', '25f9e794323b453885f5181f1b624d0b', 'niko@letos', '', '1234566', 'Εργαζόμενος'),
-('Ko', '25f9e794323b453885f5181f1b624d0b', 'niko@letos', '', '1234566', 'Εργαζόμενος');
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`afm`);
+
+--
+-- Indexes for table `employee_action`
+--
+ALTER TABLE `employee_action`
+  ADD UNIQUE KEY `id` (`action_id`);
+
+--
+-- Indexes for table `employer`
+--
+ALTER TABLE `employer`
+  ADD PRIMARY KEY (`afm`);
+
+--
+-- Indexes for table `genericuser`
+--
+ALTER TABLE `genericuser`
+  ADD PRIMARY KEY (`afm`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`afm`) REFERENCES `genericuser` (`afm`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
